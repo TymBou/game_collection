c4283d99b66335ba82da3b59456126b07ce4d7cb
Maxlamenace572 <maxence.barriergallet@gmail.com>
2023-12-22 10:03:31 +0100

    connexion view ajout

--- models/connexionModel.php
+++ models/connexionModel.php
@@ -1,11 +1,17 @@
 <?php
-function connectionSite($idUser, $passwordHash)
+function connectionSite($email, $passwordHash)
 {
+    $idUser = idForEmail($email);
     $passwordHashBDD = select("SELECT mdp FROM utilisateur WHERE idUtil = '$idUser';")[0]["mdp"];
     if (password_verify($passwordHash, $passwordHashBDD)) {
         $_SESSION["user"] = $idUser;
-      } else {
+    } else {
         echo 'erreur';
-      }
+    }
+}
+
+function idForEmail($email)
+{
+    return select("SELECT idUtil FROM utilisateur WHERE email = '$email';")[0]["idUtil"];
 }
 ?>
---
+++ views/connexionView.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    
+<form method="post">
+  <input type="text" name="name" placeholder="">
+  <input type="password" name="pssw" placeholder="">
+  
+  <input type="submit" name="submit" value="SE CONNECTER">
+</form>
+
+</body>
+</html>
