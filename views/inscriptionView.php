2566e2413c53e7403096268ecc329a46c0cd30cd
Maxlamenace572 <maxence.barriergallet@gmail.com>
2023-12-22 11:59:58 +0100

    [inscription] view ajout

---
+++ views/inscriptionView.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    
+<form method="post">
+  <input type="text" name="lastName">
+  <input type="text" name="firstName">
+  <input type="email" name="email">
+  <input type="password" name="pwd">
+  <input type="password" name="pwdConf">
+  
+  <input type="submit" name="submit" value="S'INSCRIRE">
+  <a href="">Se connecter</a>
+</form>
+
+</body>
+</html>
