baf5ef5e46d9ce320c617a4cffbd836a1055b771
Maxlamenace572 <maxence.barriergallet@gmail.com>
2023-12-22 11:10:45 +0100

    [connexion] env

--- controllers/connexionController.php
+++ controllers/connexionController.php
@@ -1,12 +1,16 @@
 <?php
-session_start();
+session_start(); //cette ligne et celle d'après à retirer si mis dans un autre controller
+require '../models/model.php';
 
-require './models/connexionModel.php';
+require '../models/connexionModel.php';
 
 if (isset($_POST["email"]) && isset($_POST["pwd"])) {
     $email = htmlspecialchars($_POST["email"]);
     $pwd =  password_hash(htmlspecialchars($_POST["pwd"]), PASSWORD_BCRYPT);
 
     connectionSite($email, $pwd);
+    echo "connect " . $_SESSION['user'];
 }
+
+require '../views/connexionView.php';
 ?>
