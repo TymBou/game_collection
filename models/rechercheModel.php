a7e54427cbd661c7f55dd4839077317a8be2faf8
Maxlamenace572 <maxence.barriergallet@gmail.com>
2023-12-27 14:17:55 +0100

    [insciption] mise du env chez moi

--- models/rechercheModel.php
+++ models/rechercheModel.php
@@ -1,16 +1,4 @@
 <?php
-function connectionBDD()
-{
-    return new PDO("mysql:host=" . $_ENV['HOST'] . ";dbname=" .$_ENV['DBNAME'], $_ENV['USER'], $_ENV['PASSWORD'], array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
-}
-
-function select($query)
-{
-  $res  = connectionBdd()->query($query);
-  $table = $res->fetchALL(PDO::FETCH_ASSOC);
-  return $table;
-}
-
 function getAllGames() {
     return select('SELECT nomJeu, URLCover, plateforme FROM jeu');
 }
