4b0eaf7f2d685c253785fd349effba03d8e78716
Maxlamenace572 <maxence.barriergallet@gmail.com>
2023-12-28 18:59:40 +0100

    [profil] deconnection

--- controllers/profilController.php
+++ controllers/profilController.php
@@ -8,5 +8,17 @@ $lName = $infos['nomUtil'];
 $fName = $infos['prenom'];
 $email = $infos['email'];
 
+if(isset($_GET['action'])) {
+    $action = htmlspecialchars($_GET['action']);
+    if($action == 'modifier') {
+        
+    } elseif($action == 'supprimer') {
+
+    } elseif($action == 'deconnecter') {
+        session_destroy();
+        session_unset();
+    }
+}
+
 require './views/profilView.php';
 ?>
