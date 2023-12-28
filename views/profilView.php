807db1db2d369d1d705ff16485c6d99ffad1950a
Maxlamenace572 <maxence.barriergallet@gmail.com>
2023-12-28 18:09:00 +0100

    [profil] add view

---
+++ views/profilView.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Connexion</title>
+</head>
+<body>
+    
+    <?php 
+        echo $lName;
+        echo $fName;
+        echo $email;
+    ?>
+    <a href="">MODIFIER MON PROFIL</a>
+    <a href="">SUPPRIMER MON COMPTE</a>
+    <a href="">SE DÉCONNECTER</a>
+
+
+</body>
+</html>
