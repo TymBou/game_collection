58c5ae006985c4a1ec85334c3b7ae614e210f883
Hwelf Kyx <134490297+KylLandais@users.noreply.github.com>
2024-01-14 01:28:34 +0100

    [ajout] modification

--- controllers/rechercheController.php
+++ controllers/rechercheController.php
@@ -1,12 +1,13 @@
 <?php
 
     require("models/rechercheModel.php");
-
+    
     if (isset($_POST['jeuRecherche'])) {
-        $games = getSearchedGames( htmlspecialchars($_POST['jeuRecherche']));
+        $games = getSearchedGames($_SESSION['user'], htmlspecialchars($_POST['jeuRecherche']));
     }
     else {
-        $games = getAllGames();
+        $games = getAllGames($_SESSION['user']);
     }
+    
     require("views/rechercheView.php");
 ?>
--- models/rechercheModel.php
+++ models/rechercheModel.php
@@ -1,10 +1,9 @@
 <?php
-function getAllGames() {
-    return select('SELECT idJeu, nomJeu, URLCover, plateforme FROM jeu');
+function getAllGames($user) {
+    return select('SELECT DISTINCT jeu.idJeu, nomJeu, URLCover, plateforme FROM jeu LEFT JOIN bibliotheque ON bibliotheque.idJeu = jeu.idJeu WHERE jeu.idJeu NOT IN (SELECT jeu.idJeu FROM jeu LEFT JOIN bibliotheque ON bibliotheque.idJeu = jeu.idJeu WHERE idUtil = ' . $user . ')' );
 }
 
-function getSearchedGames($nomGame) {
-    return select("SELECT idJeu, nomJeu, URLCover, plateforme FROM jeu WHERE nomJeu LIKE '%". $nomGame. "%'");
-
+function getSearchedGames($user, $nomGame) {
+    return select('SELECT DISTINCT jeu.idJeu, nomJeu, URLCover, plateforme FROM jeu LEFT JOIN bibliotheque ON bibliotheque.idJeu = jeu.idJeu WHERE jeu.idJeu NOT IN (SELECT jeu.idJeu FROM jeu LEFT JOIN bibliotheque ON bibliotheque.idJeu = jeu.idJeu WHERE idUtil = "' . $user . '") AND nomJeu LIKE "%'. strtolower($nomGame) . '%"');
 }
 ?>
