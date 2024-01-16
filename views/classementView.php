e59c282d52b6db27d1889033b505c518f74e312e
Hwelf Kyx <134490297+KylLandais@users.noreply.github.com>
2024-01-16 18:37:14 +0100

    [classement] creation fichier

---
+++ views/classementView.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+  <meta charset="UTF-8">
+  <link href="./css/style.css" rel="stylesheet" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+  <title>Connexion</title>
+</head>
+
+<body>
+
+  
+  
+</body>
+<footer>
+  Game Collection - 2023 - Tout droits réservés
+</footer>
+
+</html>
