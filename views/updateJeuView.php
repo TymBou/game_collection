54c8e77cf38fdc9cbeb1ab68a1177f6e598dfc1a
Tymothé BOUGEANT <tyty.bougeant@gmail.com>
2024-01-17 17:24:49 +0100

    [CSS] oubli de la page update jeu

--- views/updateJeuView.php
+++ views/updateJeuView.php
@@ -10,23 +10,23 @@
 
 <body>
     <main>
-        <div>
-            <div>
-                <a href="<?php echo $game['URLSite'] ?>"><img src="<?php echo $game['URLCover'] ?>"></a>
-            </div>
-            <div>
-                <div>
+        <div style="margin-left: 5%">
+            <div class="flexcontainer">
+                <div class="flexchild">
                     <h2><?php echo $game['nomJeu'] ?></h2>
                     <p><?php echo $game['plateforme'] ?></p>
                     <p>Temps passé : <?php echo $game['heureDeJeu'] ?> H </p>
-                </div>
-                <div>
                     <h2>Ajouter du temps passé sur le jeu</h2>
                     <form method='POST'>
+                        <p>Temps passé sur le jeu</p>
                         <input type="number" name="nbrHeure">
-                        <input type="submit">
+                        <br \>
+                        <input class="btn btnNoMargin" type="submit" value="Ajouter">
                     </form>
-                    <a href="?dir=update&jeu=<?php echo $game['idJeu'] ?>&action=supprimer">Supprimer le jeu de ma bibliothèque</a>
+                    <a class="btn" href="?dir=update&jeu=<?php echo $game['idJeu'] ?>&action=supprimer">Supprimer le jeu de ma bibliothèque</a>
+                </div>
+                <div class="flexchild">
+                    <a href="<?php echo $game['URLSite'] ?>"><img src="<?php echo $game['URLCover'] ?>"></a>
                 </div>
             </div>
         </div>
